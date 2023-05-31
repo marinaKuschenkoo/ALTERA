d9821605d1bdd4a8552297ee6f564167a097eb19
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-06-01 00:23:11 +0300

    добавила кейсы

--- cases_headers.js
+++ cases_headers.js
@@ -1,43 +1,4 @@
 const elementTemplate = document.querySelector('#element');
-const visualCards = [
-    {
-     /* link: ".\img\cases\visual_1.JPG"*/
-      link:"https://i.postimg.cc/6qYZnkjC/visual-1.jpg"
-    },
-    {
-      link: "https://i.postimg.cc/G3QGWDL9/visual-2.jpg"
-    },
-    {
-      link: "https://i.postimg.cc/GhmGdptK/visual-3.jpg"
-    },
-    {
-      link: "https://i.postimg.cc/9Q49wNGq/visual-4.jpg"
-    },
-    {
-         link:"https://i.postimg.cc/YqDg9wmH/visual-5.jpg"
-       },
-       {
-         link: "https://i.postimg.cc/2SPZQFw9/visual-6.jpg"
-       },
-       {
-         link: "https://i.postimg.cc/6QnGY2bW/visual-7.jpg"
-       },
-       {
-         link: "https://i.postimg.cc/Twc5X8XB/visual-8.jpg"
-       },
-       {
-        link: "https://i.postimg.cc/RhV6SPSh/visual-9.jpg"
-      },
-      {
-        link: "https://i.postimg.cc/Jn8HgrSb/visual-10.jpg"
-      },
-      {
-        link: "https://i.postimg.cc/TY1LBHNH/visual-11.jpg"
-      },
-      {
-        link: "https://i.postimg.cc/SNP247wQ/visual-12.jpg"
-      },
-  ]; 
   const shapkaCards = [
     {
       link:"https://i.postimg.cc/5Ns5CCTD/shapka-1.jpg"
@@ -105,7 +66,7 @@ const renderCard = (link)=>{
 elements.append(addItem(link));
 }
 function displayCards(){
-  visualCards.forEach(function(item){
+  shapkaCards.forEach(function(item){
   renderCard(item.link);
 })
 }
